b8e91303afd1dab0cfdff32fec59fcfd40a030e8
fgsaaa <1072134429@qq.com>
2024-01-19 02:19:39 +0800

    TEST

--- src/test/java/com/springboot/Chapter43ApplicationTests.java
+++ src/test/java/com/springboot/Chapter43ApplicationTests.java
@@ -1,17 +1,21 @@
 package com.springboot;
 
+import com.springboot.repsitory.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Chapter43ApplicationTests {
-
+    @Autowired
+    UserRepository userRepository;
     @Test
     public void contextLoads() {
         System.out.println(1111);
+        System.out.println(userRepository.findById(27L).get());
     }
 
 }
